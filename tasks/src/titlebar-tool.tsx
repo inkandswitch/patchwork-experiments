43b2b57013f2fab818a7978e0d158cea769c913b
Alessandro Warth <alexwarth@gmail.com>
2025-12-05 13:39:32 -0800

    task queue titlebar's humble beginnings

--- tasks/src/titlebar-tool.tsx
+++ tasks/src/titlebar-tool.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { DocHandle } from '@automerge/automerge-repo';
-import { RepoContext } from '@automerge/automerge-repo-react-hooks';
+import { AutomergeUrl, DocHandle } from '@automerge/automerge-repo';
+import { RepoContext, useDocument } from '@automerge/automerge-repo-react-hooks';
+import { TaskQueue } from './datatype';
+import type { OpenDocumentEventDetail } from "@inkandswitch/patchwork-elements";
 
-const TitlebarToolComponent: React.FC<{ docUrl: string }> = () => {
+const TitlebarToolComponent: React.FC<{ element: HTMLElement, docUrl: AutomergeUrl }> = ({ element, docUrl }) => {
+  const [doc] = useDocument<TaskQueue>(docUrl, { suspense: true });
+
   return (
-    <div className="h-full flex items-center">
-      <span>PQ</span>
+    <div className="h-full flex items-center" onClick={() => element.dispatchEvent(createOpenEvent({ url: docUrl }))}>
+      <span>{doc.title ?? 'TQ'}</span>
+      <span>{doc.pending.length}</span>
+      /
+      <span>{doc.done.length}</span>
     </div>
   );
 };
@@ -16,9 +23,17 @@ export const TitlebarTool = (handle: DocHandle<unknown>, element: HTMLElement) =
   const root = createRoot(element);
   root.render(
     <RepoContext.Provider value={repo}>
-      <TitlebarToolComponent docUrl={handle.url} />
+      <TitlebarToolComponent element={element} docUrl={'automerge:38YYaP6izqpmgUcTK2NNhmDJj1fD' as AutomergeUrl} />
     </RepoContext.Provider>
   );
   return () => root.unmount();
 };
 
+function createOpenEvent(detail: OpenDocumentEventDetail) {
+  const openEvent = new CustomEvent("patchwork:open-document", {
+    detail,
+    bubbles: true,
+    composed: true,
+  });
+  return openEvent;
+}
