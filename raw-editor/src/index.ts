e902def0002677d7df44dc325743e345cd9173bc
grjte <grjte@protonmail.com>
2026-01-29 15:11:53 +0000

    fix: typo and formatting in index

--- raw-editor/src/index.ts
+++ raw-editor/src/index.ts
@@ -1,18 +1,18 @@
 import type {
-	LoadablePlugin,
-	ToolDescription,
-	ToolImplementation,
-} from "@inkandswitch/patchwork-plugins"
+  LoadablePlugin,
+  ToolDescription,
+  ToolImplementation,
+} from "@inkandswitch/patchwork-plugins";
 
 export const plugins: LoadablePlugin<any>[] = [
-	{
-		type: "patchwork:tool",
-		id: "raw",
-		name: "Raw",
-		supportedDataTypes: "*",
-		async load() {
-			const {TinyTool} = await import("./components/RawEditor")
-			return TinyTool
-		},
-	} satisfies LoadablePlugin<ToolDescription, ToolImplementation>,
-]
+  {
+    type: "patchwork:tool",
+    id: "raw",
+    name: "Raw",
+    supportedDatatypes: "*",
+    async load() {
+      const { TinyTool } = await import("./components/RawEditor");
+      return TinyTool;
+    },
+  } satisfies LoadablePlugin<ToolDescription, ToolImplementation>,
+];
