67ad32bcbfe58ad4ad7cb1db82913c478d52b131
Alessandro Warth <alexwarth@gmail.com>
2026-02-05 00:47:39 +0100

    tasks: refactoring

--- tasks/src/helpers.ts
+++ tasks/src/helpers.ts
@@ -1,15 +1,23 @@
-import { AutomergeUrl, isValidAutomergeUrl, Repo } from '@automerge/automerge-repo/slim';
+import { AutomergeUrl, isValidAutomergeUrl, DocHandle, Repo } from '@automerge/automerge-repo/slim';
 
-export async function getSelfContactUrl(repo: Repo): Promise<AutomergeUrl | null> {
+// TODO: where's the type for account??
+export async function getAccountHandle(repo: Repo): Promise<DocHandle<any>> {
   const accountDocUrl = localStorage.getItem('tinyPatchworkAccountUrl');
   if (!isValidAutomergeUrl(accountDocUrl)) {
-    console.log('account doc url invalid', accountDocUrl);
-    return null;
+    const errorMsg = `account doc url invalid: ${accountDocUrl}`;
+    console.error(errorMsg);
+    throw new Error(errorMsg);
   }
   const accountHandle = await repo.find<any>(accountDocUrl);
   if (!accountHandle) {
-    console.log('no doc at account doc url', accountDocUrl);
-    return null;
+    const errorMsg = `no doc at account doc url: ${accountDocUrl}`;
+    console.error(errorMsg);
+    throw new Error(errorMsg);
   }
+  return accountHandle;
+}
+
+export async function getSelfContactUrl(repo: Repo): Promise<AutomergeUrl> {
+  const accountHandle = await getAccountHandle(repo);
   return accountHandle.doc().contactUrl;
 }
