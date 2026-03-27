1be05e505546531fa11705346a85c7fab9b9e686
Alessandro Warth <alexwarth@gmail.com>
2026-03-26 18:18:06 -0700

    pyonpyon: flesh out the document type

--- pyonpyon/src/datatype.ts
+++ pyonpyon/src/datatype.ts
@@ -1,12 +1,31 @@
 import type { DatatypeImplementation } from '@inkandswitch/patchwork-plugins';
-import type { PyonpyonDoc } from './types';
+import type { ObjectTable, PyonpyonDoc } from './types';
 
 export const PyonpyonDatatype: DatatypeImplementation<PyonpyonDoc> = {
   init(doc: PyonpyonDoc) {
     doc['@patchwork'] = { type: 'pyonpyon' };
     doc.title = 'Untitled';
+    doc.objProtoId = 0;
+    doc.wId = 1;
+    doc.objectTable = {
+      // obj proto
+      0: {
+        type: 'obj',
+        id: 0,
+        props: {
+          toString: { type: 'ref', id: 2 },
+        } as any,
+      },
+      // w
+      1: { type: 'obj', id: 1, protoId: 0, props: {} },
+      // obj proto's toString()
+      2: { type: 'fun', id: 2, code: '() => "[obj]"' },
+    } satisfies ObjectTable;
   },
   getTitle(doc: PyonpyonDoc) {
     return doc.title?.trim() || 'Pyonpyon';
+  },
+  setTitle(doc: PyonpyonDoc, title: string) {
+    doc.title = title.trim();
   },
 };
--- pyonpyon/src/types.ts
+++ pyonpyon/src/types.ts
@@ -1,4 +1,50 @@
 export type PyonpyonDoc = {
   '@patchwork': { type: 'pyonpyon' };
   title: string;
+  objProtoId: number;
+  wId: number;
+  objectTable: ObjectTable;
 };
+
+export type Obj = Record<string, any>;
+
+export type SVal = number | boolean | string | Ref | null;
+
+export type SObj = {
+  type: 'obj';
+  id: number;
+  protoId?: number;
+  props: Record<string, SVal>;
+};
+
+export type SFun = {
+  type: 'fun';
+  id: number;
+  code: string;
+};
+
+export type SArr = {
+  type: 'arr';
+  id: number;
+  elements: SVal[];
+};
+
+export type SSet = {
+  type: 'set';
+  id: number;
+  elements: SVal[];
+};
+
+export type SMap = {
+  type: 'map';
+  id: number;
+  keys: SVal[];
+  values: SVal[];
+};
+
+export type Ref = {
+  type: 'ref';
+  id: number;
+};
+
+export type ObjectTable = Record<number, SObj | SFun | SArr | SSet | SMap>;
