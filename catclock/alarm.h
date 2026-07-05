0a08bcb355e2bbf164a59ffde6f6a72fc5f7a192
chee <yay@chee.party>
2026-07-05 17:38:32 +0100

    remove catclock

--- catclock/alarm.h
+++
@@ -1,9 +0,0 @@
-/*
- *  alarm.h -- externs
- */
-extern void InitBellAlarm(Window, int, int, XFontStruct *, XmFontList, Pixel, Pixel, Boolean *, Boolean *);
-extern void SetAlarm(char *);
-extern void SetBell(int);
-extern void GetBellSize(int *, int *);
-extern void AlarmOff(void);
-extern void DrawBell(int);
