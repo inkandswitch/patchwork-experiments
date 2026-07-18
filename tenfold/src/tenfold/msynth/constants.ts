f36d91e260a0b6f6e0cb0fc93c0c3e8e1faa3788
chee <yay@chee.party>
2026-07-18 14:45:10 +0200

    remove patternwith and tenfold from here, they live elsewhere
    patternwitch is in base

--- tenfold/src/tenfold/msynth/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SAMPLE_RATE = 48_000;
-export const SAMPLE_RATE_RECIP = 1 / SAMPLE_RATE;
-export const TAU = 2 * Math.PI;
--- tenfold/src/tenfold/msynth/types.ts
+++
@@ -1,9 +0,0 @@
-export type MessageToWorklet =
-  | {
-      command: 'load patch';
-      code: string;
-      params: SharedArrayBuffer;
-    }
-  | { command: 'process midi message'; data: Uint8Array };
-
-export type MessageFromWorklet = { event: 'log'; message: string };
